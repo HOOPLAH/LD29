85a0c394ae5a7a303057440db86c6df9335173e8
unknown <lanpartyadmin@CH11-11.csdept.uhhilo>
2014-04-26 19:28:58 -1000

    added waypoints

--- World.lua
+++ World.lua
@@ -1,28 +1,32 @@
 require 'Player'
-require 'Building'
+require 'Point'
 local class = require 'middleclass'
 
 World = class('World')
 
 function World:initialize()
-	self.lastMadeBuilding = Building:new(128, -1, 'Content/Textures/ammocrate.png')
-	self.buildings = {self.lastMadeBuilding}
+	self.lastMadePoint = Point:new(0, 0)
+	self.points = {self.lastMadePoint}
 end
 
-function World:createNewBuilding()
-	local random_y = math.abs(love.math.random(self.lastMadeBuilding.y, self.lastMadeBuilding.y+128))
-	table.insert(self.buildings, Building:new(self.lastMadeBuilding.x+128, -random_y, 'Content/Textures/ammocrate.png'))
-	self.lastMadeBuilding = self.buildings[table.getn(self.buildings)-1]
+function World:createNewPoint()
+	local random_y = math.abs(love.math.random(self.lastMadePoint.y, self.lastMadePoint.y+256))
+	table.insert(self.points, Point:new(self.lastMadePoint.x+64, -random_y))
+	self.lastMadePoint = self.points[table.getn(self.points)]
 end
 
 function World:draw()
-	for _, v in ipairs(self.buildings) do
-		love.graphics.draw(v:getImage(), v:getX(), v:getY())
+	for i, v in ipairs(self.points) do
+		love.graphics.rectangle("fill", v:getX(), v:getY(), 4, 4)
+		if i > 1 then
+			love.graphics.line(self.points[i-1]:getX(), self.points[i-1]:getY(), v:getX(), v:getY())
+		end
+		--love.graphics.line( x1, y1, x2, y2, ... )
 	end
 end
 
 function World:update(dt)
-	if player.x > self.lastMadeBuilding.x then
-		self:createNewBuilding()
+	if player.x > self.lastMadePoint.x then
+		self:createNewPoint()
 	end
 end
--- main.lua
+++ main.lua
@@ -15,13 +15,11 @@ function love.load()
 	
 	--create player
 	player = Player:new()
-	test  = Building:new(64, 64, "Content/Textures/ammocrate.png")
 end
 
 function love.draw()
 	camera:set()
 	player:draw()
-	love.graphics.draw(test.img, test.x, test.y)
 	world:draw()
 	camera:unset()
 	camera:draw()
@@ -31,10 +29,6 @@ end
 
 function love.update(dt)
 	player:update(dt)
-    
-    if check_collision(player, test) then
-		handle_collision(player, test)
-	end
     
 	camera:setCenter(player:get_center_x(), player:get_center_y())
     
